8e7aa1ffdb555f60beb02d935b4d5c81df9a98f6
Cristiano M <devcristianomorais@gmail.com>
2023-03-27 19:22:57 -0300

    first commit

---
+++ app/src/main/java/br/com/envolvedesenvolve/batalhademcs/view/AboutFragment.java
@@ -0,0 +1,20 @@
+package br.com.envolvedesenvolve.batalhademcs.view;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+import br.com.envolvedesenvolve.batalhademcs.R;
+
+public class AboutFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_about, container, false);
+    }
+}
