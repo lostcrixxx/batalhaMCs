c79ca91ec6b8fdc9fcec2ab795f436286347788c
Cristiano M <devcristianomorais@gmail.com>
2023-03-27 21:46:23 -0300

    layout rank

--- app/src/main/java/br/com/envolvedesenvolve/batalhademcs/view/RankFragment.java
+++ app/src/main/java/br/com/envolvedesenvolve/batalhademcs/view/RankFragment.java
@@ -1,9 +1,11 @@
 package br.com.envolvedesenvolve.batalhademcs.view;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import androidx.fragment.app.Fragment;
 
@@ -11,10 +13,20 @@ import br.com.envolvedesenvolve.batalhademcs.R;
 
 public class RankFragment extends Fragment {
 
+    private View view;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_rank, container, false);
+        view = inflater.inflate(R.layout.fragment_rank, container, false);
+
+        ImageView boxFirst = view.findViewById(R.id.img_box_first);
+        boxFirst.setBackgroundColor(Color.YELLOW);
+        ImageView boxTwo = view.findViewById(R.id.img_box_two);
+        boxTwo.setBackgroundColor(Color.YELLOW);
+        ImageView boxTree = view.findViewById(R.id.img_box_tree);
+        boxTree.setBackgroundColor(Color.YELLOW);
+
+        return view;
     }
 }
