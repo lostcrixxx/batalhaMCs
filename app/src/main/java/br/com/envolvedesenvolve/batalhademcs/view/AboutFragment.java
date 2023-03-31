eec0043b827cda146f6261770f3a8a133b2f244c
Cristiano M <devcristianomorais@gmail.com>
2023-03-30 23:40:21 -0300

    added version app

--- app/src/main/java/br/com/envolvedesenvolve/batalhademcs/view/AboutFragment.java
+++ app/src/main/java/br/com/envolvedesenvolve/batalhademcs/view/AboutFragment.java
@@ -4,17 +4,28 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.fragment.app.Fragment;
 
+import br.com.envolvedesenvolve.batalhademcs.BuildConfig;
 import br.com.envolvedesenvolve.batalhademcs.R;
 
 public class AboutFragment extends Fragment {
 
+    private View view;
+    private TextView txtVersion;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_about, container, false);
+
+        view = inflater.inflate(R.layout.fragment_about, container, false);
+        txtVersion = view.findViewById(R.id.text_about);
+
+        String versionName = "versão: " + BuildConfig.VERSION_NAME;
+        txtVersion.setText(versionName);
+
+        return view;
     }
 }
